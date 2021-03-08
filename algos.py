b0b06cb0e2ddfff8bf8145b062d70cc7e4b87646
Theophile Cantelobre <theophilec@gmail.com>
2021-03-08 16:32:31 +0100

    Refactoring and updates

--- algos.py
+++ algos.py
@@ -2,7 +2,7 @@ import cvxpy as cp
 import numpy as np
 
 
-def reconstruct_1bit(y, A):
+def reconstruct_1bit(y, A, verbose=True):
     """
     Solve the modified basis pursuit problem by Plan and Vershynin.
 
@@ -21,29 +21,21 @@ def reconstruct_1bit(y, A):
     """
     m, n = A.shape
     x = cp.Variable(shape=n)
-    u = cp.Variable(shape=n)
 
     constraints = []
-    # - u_i <= x_i <= u_i
-    for i in range(n):
-        constraints += [
-            u[i] >= 0,
-            x[i] >= -u[i],
-            x[i] <= u[i],
-        ]
 
     # y_i*<a_i, x> >= 0
-    for i in range(m):
-        constraints += [
-            y[i] * cp.sum(cp.multiply(A[i], x)) >= 0,
+    constraints += [
+            cp.multiply(y, (A @ x)) >= 0,
         ]
 
     # 1/m sum() >= 1
     constraints.append(cp.sum(cp.multiply(y, A @ x)) / m >= 1)
 
-    objective = cp.Minimize(cp.sum(u))
+    objective = cp.Minimize(cp.norm1(x))
     prob = cp.Problem(objective, constraints)
-    prob.solve()
+    print("Calling solver.")
+    prob.solve(verbose=verbose)
 
     return x.value
 
--- utils.py
+++ utils.py
@@ -1,17 +1,44 @@
 import numpy as np
+from numpy import pi as PI
+from numpy.linalg import norm as norm2
 import matplotlib.pyplot as plt
 
 
 def sign(v):
+    """
+    Compute component-wise sign of vector.
+
+    Zero handling: zero is positive by convention.
+
+    Params:
+        v: ndarray
+    """
     return np.where(v < 0, -1.0, 1.0)
 
 
-def show_unit_ball(img):
-    plt.imshow(img.reshape(28, 28) / np.linalg.norm(img))
+def img_show_unit_ball(img):
+    """
+    Show image reshaping as square and normalizing on unit sphere.
+
+    Params:
+        img: (n,) ndarray
+    """
+    side = int(np.sqrt(img.shape[0]))
+    plt.imshow(img.reshape(side, side) / np.linalg.norm(img))
     plt.show()
 
 
 def generate_sparse_randn(n, s):
+    """
+    Generate sparse signal with Normal components.
+
+    Signal is of length n, with sparsity s.
+    Components are iid Normal.
+
+    Params:
+        n: int, length of signal
+        s: int, sparsity level. 0-norm of signal.
+    """
     x = np.zeros(n)
     I = np.random.randint(0, n, s)
     x[I] = np.random.randn(s)
@@ -19,7 +46,69 @@ def generate_sparse_randn(n, s):
 
 
 def generate_sparse(n, s):
+    """
+    Generate sparse signal with 1 values.
+
+    Signal is of length n, with sparsity s.
+    Components are equal to 1.
+
+    Params:
+        n: int, length of signal
+        s: int, sparsity level. 0-norm of signal.
+    """
     x = np.zeros(n)
     I = np.random.randint(0, n, s)
     x[I] = 1
     return x
+
+def angular_error(v_est, v_true):
+    """
+    Compute angular error as in [18] (Section 3.1).
+
+    Quantity is defined as:
+        1 / pi arccos(<v_est, v_true>)
+
+    Params:
+        v_est: (n,) ndarray
+        v_true: (n,) ndarray
+
+    Note: v_est and v_true must have same size.
+    """
+    assert(v_est.shape == v_true.shape)
+
+    return 1 / PI * np.arccos(np.sum(v_est * v_true))
+
+
+def hamming_distance(v_est, v_true):
+    """
+    Compute Hamming distance as in [18] (Section 3.1).
+
+    Quantity is defined as:
+        1 / n * sum XOR(v_est[i], v_true[i])
+
+    Params:
+        v_est: (n,) ndarray
+        v_true: (n,) ndarray
+
+    Note: v_est and v_true must have same size.
+    """
+    assert(v_est.shape == v_true.shape)
+
+    return 1 / len(v_est) * np.sum(v_est != v_true)
+
+def snr(v_est, v_true):
+    """
+    Compute SNR as in [18] (p. 17).
+
+    Quantity is defined as:
+        10 log_10(||v_true||_2^2 / ||v_est - v_true||_2^2)
+
+    Params:
+        v_est: (n,) ndarray
+        v_true: (n,) ndarray
+
+    Note: v_est and v_true must have same size.
+    """
+    assert(v_est.shape == v_true.shape)
+
+    return 10 * np.log10(norm2(v_true) ** 2 / norm2(v_est - v_true) ** 2)
